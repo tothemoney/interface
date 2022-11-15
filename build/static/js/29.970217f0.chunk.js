915962713a778847b493e7d3656ca1c425d9b141
tmy-redesign <tmy-redesign@tmy>
2022-11-15 19:36:01 +0400

    что-то еще

--- build/static/js/29.970217f0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[29],{1359:function(s,e){s.exports={messages:{}}}}]);
-//# sourceMappingURL=29.970217f0.chunk.js.map
